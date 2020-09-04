431ccbea3e2ed797f9022a720ff9f0b724bc25d7
carlos-nunez-peces <carlos26102@gmail.com>
2020-09-04 07:55:22 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { StudentListModule } from './modules/student-list/student-list.module';
+import { StudentDetailModule } from './modules/student-detail/student-detail.module';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // App Modules
+    StudentListModule,
+    StudentDetailModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
